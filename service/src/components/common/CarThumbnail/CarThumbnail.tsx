644dd9eef4deb383824028f4e853da91ed8ba622
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-26 16:17:48 +0900

    fix : background image url 삽입

--- service/src/components/common/CarThumbnail/CarThumbnail.tsx
+++ service/src/components/common/CarThumbnail/CarThumbnail.tsx
@@ -1,11 +1,16 @@
 interface CarThumbnailProps {
   subtitle: string;
   description: string;
+  backgroundImage: string;
 }
 
-const CarThumbnail = ({ subtitle, description }: CarThumbnailProps) => {
+const CarThumbnail = ({ subtitle, description, backgroundImage }: CarThumbnailProps) => {
   return (
-    <div className="w-[49rem] h-[26.5rem] absolute z-10 bg-white flex flex-col justify-between">
+    <div
+      className="w-[49rem] h-[26.5rem] absolute z-10 bg-cover bg-center bg-no-repeat flex flex-col justify-between"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      {" "}
       <div className="flex justify-end mt-12 mr-12 text-white">
         <div className="bg-green-400 py-2 px-[0.65rem]">자세히 보기</div>
       </div>
@@ -13,7 +18,7 @@ const CarThumbnail = ({ subtitle, description }: CarThumbnailProps) => {
         <div className="text-left font-pretendard text-xl">{subtitle}</div>
         <div className="text-left font-pretendard text-[0.875rem]">{description} </div>
       </div>
-      <div className="h-[80%] bg-gradient-to-b from-transparent via-opacity-60 to-gray-600"></div>
+      <div className="h-[80%] bg-gradient-to-b from-transparent via-opacity-20 to-gray-600"></div>
     </div>
   );
 };
