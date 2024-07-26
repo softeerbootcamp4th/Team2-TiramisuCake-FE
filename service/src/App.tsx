6d6b780cb5aaf01c37fec9dc445db7bf77ad59f5
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-26 11:02:37 +0900

    feat : 화면 기본 레이아웃 구성

--- service/src/App.tsx
+++ service/src/App.tsx
@@ -1,9 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import Header from "./components/common/Header/Header";
+import EventInfoFooter from "./components/common/Footer/EventInfoFooter";
+import Footer from "./components/common/Footer/Footer";
+import Tooltip from "./components/common/Tooltip/Tooltip";
 
 function App() {
   return (
     <>
-      <Outlet />
+      <div className="relative">
+        <Header />
+        <main className="pt-14">
+          <Outlet />
+        </main>
+        <EventInfoFooter />
+        <Footer />
+        <Tooltip />
+      </div>
     </>
   );
 }
