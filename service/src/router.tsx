40ceabeac112fdbe497141affe82273a8edef42c
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-30 11:30:31 +0900

    fix : router 수정

--- service/src/router.tsx
+++ service/src/router.tsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import MainPage from './pages/MainPage';
 
 export const router = createBrowserRouter([
   {
     element: <App />,
-    path: "/",
+    path: '/',
+    children: [
+      {
+        element: <MainPage />,
+        index: true, //기본 경로
+      },
+    ],
   },
 ]);
