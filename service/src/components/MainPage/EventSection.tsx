3d0d1e67141400d2b83aa15619d6a1959b6509b7
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-30 14:03:37 +0900

    fix. 뱃지 적용 및 event info type

--- service/src/components/MainPage/EventSection.tsx
+++ service/src/components/MainPage/EventSection.tsx
@@ -1,65 +1,73 @@
-import React from "react";
-import splitSentences from "@/utils/splitSentence";
-import downArrow from "/svg/downarrow.svg";
-import { InformItem } from "@/types/eventInfoItem";
+import React from 'react';
+import splitSentences from '@/utils/splitSentence';
+import downArrow from '/svg/downarrow.svg';
+import { InformItem } from '@/types/eventInfoItem';
+import Button from '../common/Button/Button';
+import Badge from '../common/Badge/Badge';
 
 interface EventSectionProps {
-  startDate: "2024.09.02";
-  endDate: "2024.09.15";
+  startDate: '2024.09.02';
+  endDate: '2024.09.15';
 }
 
-const backgroundImage = "image158.png";
-const title = "신차 출시 기념 EVENT";
+const backgroundImage = 'image158.png';
+const title = '신차 출시 기념 EVENT';
 const description =
-  "현대자동차의 The new IONIQ 5 출시 기념 이벤트로 여러분을 초대합니다. 24시간 무료 렌트, 신차 할인 쿠폰 및 다양한 경품을 받아보세요.";
+  '현대자동차의 The new IONIQ 5 출시 기념 이벤트로 여러분을 초대합니다. 24시간 무료 렌트, 신차 할인 쿠폰 및 다양한 경품을 받아보세요.';
 
 const informs = [
   {
-    when: "매주 월,목 오전 10시 선착순 100명",
-    hint: "인테리어",
+    when: '매주 월,목 오전 10시 선착순 100명',
+    hint: '인테리어',
     title: "'24시간 내차' 이벤트",
     eventInformation:
-      "하단의 The new IONIQ 5 정보를 바탕으로 빠르게 문장 퀴즈를 맞춰. 24시간 렌트권과 신차 할인권을 얻을 수 있어요.",
-    imageUrl: ["/image 162.png", "/image 163.png"],
+      '하단의 The new IONIQ 5 정보를 바탕으로 빠르게 문장 퀴즈를 맞춰. 24시간 렌트권과 신차 할인권을 얻을 수 있어요.',
+    imageUrl: ['/image 162.png', '/image 163.png'],
   },
   {
-    추첨: "1555명",
-    남은경품: "1554개",
-    title: "매일 복권 긁고 경품 받기",
+    winner: '1555명',
+    remaining: '1554개',
+    title: '매일 복권 긁고 경품 받기',
     eventInformation:
-      "이벤트 기간 동안 추첨을 통해 아이패드 pro 11인치, 현대백화점 10만원권, 1만원권을 드려요. 일주일 연속 참여 시, 스타벅스 쿠폰을 무조건 받을 수 있어요.",
-    imageUrl: ["svg/ipad.svg", "svg/money.svg"],
+      '이벤트 기간 동안 추첨을 통해 아이패드 pro 11인치, 현대백화점 10만원권, 1만원권을 드려요. 일주일 연속 참여 시, 스타벅스 쿠폰을 무조건 받을 수 있어요.',
+    imageUrl: ['svg/ipad.svg', 'svg/money.svg'],
   },
 ];
 
 const InformCard: React.FC<InformItem> = (props) => {
   return (
     <div className="w-[36.5rem] h-[21.8rem] flex flex-col items-center justify-between p-6 border border-custom-white bg-gradient-to-b from-white/20 to-white/10 shadow-custom backdrop-blur-custom">
-      <div className="w-full text-left">
+      <div className="w-full">
         {props.when && props.hint && (
-          <p className="text-b-s text-green-500 text-center">
-            {props.when} | 힌트: {props.hint}
-          </p>
-        )}
-        {props.추첨 && props.남은경품 && (
-          <div className="flex text-b-s text-green-500 text-center justify-center">
-            <span className="mx-3">추첨 {props.추첨} </span>
-            <span>남은 경품 {props.남은경품}</span>
+          <div className="text-center inline-flex flex-row justify-center gap-2">
+            <Badge type="lightblue" text={props.when} />
+            <Badge type="white" text={`힌트: ${props.hint}`} />
           </div>
         )}
-        <h2 className=" text-b-xxl font-bold mt-2 mb-4 text-center text-gray-800">{props.title}</h2>
+        {props.winner && props.remaining && (
+          <div className="text-center inline-flex flex-row justify-center gap-2">
+            <Badge type="lightblue" text={`추첨 ${props.winner}`} />
+            <Badge type="lightblue" text={`남은 경품 ${props.remaining}`} />
+          </div>
+        )}
+        <h2 className=" text-b-xxl font-bold mt-2 mb-4 text-center text-gray-800">
+          {props.title}
+        </h2>
         <p className="text-sm font-Pretendard text-d-s self-stretch text-gray-600 text-center">
           {splitSentences(props.eventInformation)}
         </p>
       </div>
       <div className="flex justify-center items-center w-full h-full">
         {props.imageUrl.map((img, index) => (
-          <div key={index} className="flex justify-center items-center w-full h-full">
+          <div
+            key={index}
+            className="flex justify-center items-center w-full h-full"
+          >
             <img
               src={img}
               alt={`image-${index}`}
               className="mx-4"
-              style={{ width: "246px", height: "134px", objectFit: "contain" }}
+              style={{ width: '246px', height: '134px', objectFit: 'contain' }}
             />
           </div>
         ))}
@@ -68,6 +76,9 @@ const InformCard: React.FC<InformItem> = (props) => {
   );
 };
 
+const handleLogin = () => {
+  console.log('login modal 해야 함');
+};
 const EventSection = ({ startDate, endDate }: EventSectionProps) => {
   return (
     <div
@@ -83,9 +94,15 @@ const EventSection = ({ startDate, endDate }: EventSectionProps) => {
             {title}
           </div>
         </div>
-        <div className=" font-Pretendard font-normal text-gray-800 text-center">{splitSentences(description)}</div>
+        <div className=" font-Pretendard font-normal text-gray-800 text-center">
+          {splitSentences(description)}
+        </div>
         <div className="flex my-6 py-2 px-3">
-          <button>번호 인증하고 이벤트 참여하기</button>
+          <Button
+            type="square"
+            text="번호 인증하고 이벤트 참여하기"
+            handleClick={handleLogin}
+          ></Button>
         </div>
         <div className="flex items-center flex-row text-center">
           {informs.map((inform, index) => (
