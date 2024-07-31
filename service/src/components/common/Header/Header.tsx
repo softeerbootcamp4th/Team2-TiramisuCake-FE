443fd07d5639101e0bbb337afb898936d275bd73
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-31 10:18:42 +0900

    feat : Header의 tab 클릭 시 스크롤 기능 구현

--- service/src/components/common/Header/Header.tsx
+++ service/src/components/common/Header/Header.tsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link } from 'react-router-dom';
+import { useTabContext } from '@/hooks/useTabContext';
 
 const Header = () => {
-  const [activeTab, setActiveTab] = useState("");
+  const { activeTab, setActiveTab } = useTabContext();
 
   const handleTabClick = (tabName: string) => {
     setActiveTab(tabName);
@@ -13,26 +13,26 @@ const Header = () => {
       <div className="flex items-center">
         <Link to="/">
           <img
-            onClick={() => handleTabClick("")}
+            onClick={() => handleTabClick('')}
             src="image 113.svg"
             alt="Hyundai logo"
             className="flex-shrink-0 px-2 ml-10"
-            style={{ width: "8.3125rem", height: "1.0625rem" }}
+            style={{ width: '8.3125rem', height: '1.0625rem' }}
           />
         </Link>
       </div>
       <nav className="flex space-x-12 mr-20 font-Pretendard font-bold">
         <Link
           to="/"
-          onClick={() => handleTabClick("event")}
-          className={`${activeTab === "event" ? "text-green-400" : "text-black"}`}
+          onClick={() => handleTabClick('event')}
+          className={`${activeTab === 'event' ? 'text-green-400' : 'text-black'}`}
         >
           Event
         </Link>
         <Link
           to="/"
-          onClick={() => handleTabClick("ioniq5")}
-          className={`${activeTab === "ioniq5" ? "text-green-400" : "text-black"}`}
+          onClick={() => handleTabClick('ioniq5')}
+          className={`${activeTab === 'ioniq5' ? 'text-green-400' : 'text-black'}`}
         >
           The new IONIQ 5
         </Link>
