3c4c0c3dc5a3c1b026d899488c308adcab4b2855
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-31 10:21:29 +0900

    feat : scrolling animation 적용

--- service/src/pages/MainPage.tsx
+++ service/src/pages/MainPage.tsx
@@ -1,15 +1,51 @@
-import CarInfoSection from "@/components/MainPage/CarInfoSection";
-import EventSection from "@/components/MainPage/EventSection";
-import RendingSection from "@/components/MainPage/RendingSection";
+import CarInfoSection from '@/components/MainPage/CarInfoSection';
+import EventSection from '@/components/MainPage/EventSection';
+import RendingSection from '@/components/MainPage/RendingSection';
+import { useEffect, useRef } from 'react';
+import { useTabContext } from '@/hooks/useTabContext';
 
-function MainPage() {
+const MainPage = () => {
+  const { activeTab, setActiveTab } = useTabContext();
+
+  const rendingSectionRef = useRef<HTMLDivElement>(null);
+  const eventSectionRef = useRef<HTMLDivElement>(null);
+  const carInfoSectionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let sectionRef: React.RefObject<HTMLDivElement> | null = null;
+    switch (activeTab) {
+      case 'event':
+        sectionRef = eventSectionRef;
+        break;
+      case 'ioniq5':
+        sectionRef = carInfoSectionRef;
+        break;
+      default:
+        sectionRef = rendingSectionRef;
+        break;
+    }
+    if (sectionRef && sectionRef.current) {
+      sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [activeTab]);
+
   return (
     <>
-      <RendingSection />
-      <EventSection startDate="2024.09.02" endDate="2024.09.15" />
-      <CarInfoSection />
+      <div ref={rendingSectionRef}>
+        <RendingSection onArrowClick={() => setActiveTab('event')} />
+      </div>
+      <div ref={eventSectionRef}>
+        <EventSection
+          startDate="2024.09.02"
+          endDate="2024.09.15"
+          onArrowClick={() => setActiveTab('ioniq5')}
+        />
+      </div>
+      <div ref={carInfoSectionRef}>
+        <CarInfoSection />
+      </div>
     </>
   );
-}
+};
 
 export default MainPage;
