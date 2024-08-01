eb522f04e8fd6870e57632f40968a108defa41b9
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-01 13:55:39 +0900

    feat : 7일차 스벅 당첨 애니메이션

--- service/src/components/LotteryLounge/Attendance.tsx
+++ service/src/components/LotteryLounge/Attendance.tsx
@@ -1,18 +1,28 @@
-const texts = ['I', 'O', 'N', 'I', 'Q', '5', 'Y'];
+const texts = ['I', 'O', 'N', 'I', 'Q', '5'];
 
 const Attendance = () => {
   const counts = 3;
 
   const defaultStyle = {
-    background: 'rgba(255, 255, 255, 0.8)', // 흰색의 투명도 조절
+    background: 'rgba(255, 255, 255, 0.6)', // 흰색의 투명도 조절
     width: '5rem',
     height: '5rem',
     borderRadius: '50%',
     strokeWidth: '1px',
     stroke: '#FFFFFF',
-    backdropFilter: 'blur(20px)',
+    backdropFilter: 'blur(30px)',
   };
 
+  const dynamicStyle = {
+    width: '5rem',
+    height: '5rem',
+    borderRadius: '50%',
+    background: `conic-gradient(#FFFFFF 0%, #BBE0E6 100%)`,
+    animation: `rotateCircle 1s linear 1`,
+  };
+
+  const lastTextColorClass = 6 < counts ? 'text-primary' : 'text-gray-300';
+  const lastStyle = 6 < counts ? dynamicStyle : defaultStyle;
   return (
     <div className='flex flex-col items-center gap-4 self-stretch'>
       <div className='text-center text-gray-800 font-normal text-b-m line-height-[1.125rem]'>
@@ -58,6 +68,21 @@ const Attendance = () => {
             </div>
           );
         })}
+        <div className='items-center text-center justify-center'>
+          <div className='relative flex items-center justify-center'>
+            <div className='self-stretch' style={lastStyle}>
+              <img
+                className='absolute inset-0 m-auto'
+                src={
+                  counts > 6
+                    ? '/svg/출석완료/스벅.svg'
+                    : '/svg/출석미완/스벅.svg'
+                }
+              ></img>
+            </div>
+          </div>
+          <div className={`mt-2 text-d-m ${lastTextColorClass}`}>7일차</div>
+        </div>
       </div>
     </div>
   );
