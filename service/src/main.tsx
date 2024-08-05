29ff10156f2b60e84ce81029d35887a4c36a19f4
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-05 16:57:41 +0900

    feat : login provider 구현

--- service/src/main.tsx
+++ service/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
+import { LoginProvider } from '@/store/provider/LoginProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <LoginProvider>
+      <RouterProvider router={router} />
+    </LoginProvider>
   </React.StrictMode>
 );
