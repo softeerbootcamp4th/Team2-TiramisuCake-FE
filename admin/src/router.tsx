03adcb9987a51b41dddb1aa8784a2f4a901434df
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-06 18:33:49 +0900

    chore : json 파일 추가 및 불필요 코드 제거

--- admin/src/router.tsx
+++ admin/src/router.tsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import { ROUTER_PATH } from "./lib/constants";
 import Main from "./pages/Main";
-import CommentsLounge from "./pages/CommentsLounge";
-import LotteryLounge from "./pages/LotteryLounge";
-import QuizLounge from "./pages/QuizLounge";
 import App from "./App";
 
 export const router = createBrowserRouter([
@@ -13,18 +10,6 @@ export const router = createBrowserRouter([
       {
         path: ROUTER_PATH.MAIN,
         element: <Main />,
-      },
-      {
-        path: ROUTER_PATH.COMMENTS_LOUNGE,
-        element: <CommentsLounge />,
-      },
-      {
-        path: ROUTER_PATH.LOTTERY_LOUNGE,
-        element: <LotteryLounge />,
-      },
-      {
-        path: ROUTER_PATH.QUIZ_LOUNGE,
-        element: <QuizLounge />,
       },
     ],
   },
--- service/src/components/MainPage/CarInfoSection/CarouselBg.tsx
+++ service/src/components/MainPage/CarInfoSection/CarouselBg.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const CarouselBg = ({ currentIdx }: { currentIdx: number }) => {
   return (
     <>
