a8710da07b12f3e033de87ef9226cc6cc2e72022
moana16 <moana16@naver.com>
2024-08-08 11:27:37 +0900

    style : 정렬 안맞는거 수정

--- admin/src/pages/WinManagementPage.tsx
+++ admin/src/pages/WinManagementPage.tsx
@@ -22,15 +22,15 @@ const WinManagementPage = () => {
 
   return (
     <div className='min-w-screen max-h-screen p-11'>
-      <div className=' flex  flex-col justify-evenly bg-[#F3F5F7] px-10 py-[3rem] items-center gap-10'>
+      <div className=' flex  flex-col  bg-[#F3F5F7] px-10 py-[3rem] items-center gap-10'>
         <div className='flex flex-col w-full gap-2.5 items-center'>
           <h1 className='text-4xl font-bold'>당첨 관리</h1>
           <p className='text-sm mb-1'>
             선착순 당첨 인원 수 설정 / 당첨 / 당첨자 목록 조회
           </p>
         </div>
-        <div className='flex w-full h-full'>
-          <div className='w-full'>
+        <div className='flex w-full h-full gap-10 mx-auto justify-evenly'>
+          <div className='w-full flex flex-col items-center justify-center'>
             <SetFCFSWinnerContainer
               pageType='manage'
               handleModalOpen={() => setNumberModalOpen(true)}
