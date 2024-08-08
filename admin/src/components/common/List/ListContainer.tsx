e65fd642d7be53f784a31746bccd56c44b4ef0de
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-08 13:59:59 +0900

    fix : width 값 자유롭게 변경

--- admin/src/components/common/List/ListContainer.tsx
+++ admin/src/components/common/List/ListContainer.tsx
@@ -1,14 +1,23 @@
 import { PropsWithChildren } from 'react';
 /**
- * Modal Component
  * @param Element
+ * @param width
  * @returns
  */
 
-const ListContainer = ({ children }: PropsWithChildren) => {
+interface ListContainerProps {
+  width?: string;
+}
+
+const ListContainer = ({
+  children,
+  width = '30rem',
+}: PropsWithChildren<ListContainerProps>) => {
   return (
     <div className='flex justify-center items-center mb-7'>
-      <div className='bg-white rounded-xl w-[30rem] h-fit'>{children}</div>
+      <div className='bg-white rounded-xl h-fit' style={{ width: width }}>
+        {children}
+      </div>
     </div>
   );
 };
