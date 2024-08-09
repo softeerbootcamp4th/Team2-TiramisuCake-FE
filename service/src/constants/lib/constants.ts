d904a5189824dc25b95ee0f85520997d16eebfd9
moana16 <moana16@naver.com>
2024-08-09 13:51:06 +0900

    feat : base URL, header 추가

--- service/src/constants/lib/constants.ts
+++ service/src/constants/lib/constants.ts
@@ -4,3 +4,9 @@ export const ROUTER_PATH = {
   QUIZ_LOUNGE: 'quiz-lounge',
   COMMENTS_LOUNGE: 'comments-lounge',
 } as const;
+
+export const BASE_URL = 'https://softeer.shop/';
+
+export const HEADERS = {
+  'Content-Type': 'application/json',
+};
