50d393846a6d4a8f993b01465c7a96e2a81f195f
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-10 22:25:26 +0900

    feat : cors 방지 - proxy 설정

--- service/vite.config.ts
+++ service/vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.SITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   cacheDir: './.yarn/.vite',
   resolve: {
     alias: {
