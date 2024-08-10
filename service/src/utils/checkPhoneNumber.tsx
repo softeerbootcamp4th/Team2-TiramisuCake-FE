eca4d7a8269c3d5b06297bcbce94dea3c47469ef
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-10 22:23:34 +0900

    chore : 파일 제거 및 svg 추가

--- service/src/utils/checkPhoneNumber.tsx
+++
@@ -1,5 +0,0 @@
-export const validatePhoneNumber = (value: string): boolean => {
-  // 한국 전화번호 정규식
-  const phoneNumberPattern = /^01[0-9]-\d{4}-\d{4}$/;
-  return phoneNumberPattern.test(value);
-};
