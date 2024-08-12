8bf43092ac35c7cfb003b6c6a0d5a04ab8be6c5e
moana16 <moana16@naver.com>
2024-08-12 12:09:31 +0900

    feat : query 생성

--- service/src/components/MainPage/CarInfoSection/CarInfoSection.tsx
+++ service/src/components/MainPage/CarInfoSection/CarInfoSection.tsx
@@ -1,11 +1,12 @@
 import { CarInfoProvider } from '@/store/provider/CarInfoProvider';
 import Carousel from './Carousel';
+import { useQueryGetCarDetailInfo } from '@/apis/main/query';
 const CarInfoSection = () => {
+  const { data, isLoading } = useQueryGetCarDetailInfo();
+  console.log(data);
   return (
     <CarInfoProvider>
-      <div>
-        <Carousel />
-      </div>
+      <div>{isLoading ? <div>Loading...</div> : <Carousel />}</div>
     </CarInfoProvider>
   );
 };
