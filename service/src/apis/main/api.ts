c96b1751ff3f49f1dd67e975abd6b18d91a6ffea
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-13 00:45:26 +0900

    chore : 서버 url -> env 파일에서 불러오기

--- service/src/apis/main/api.ts
+++ service/src/apis/main/api.ts
@@ -1,4 +1,6 @@
+const baseURL = `${import.meta.env.VITE_SITE_URL}`;
+
 export const getCarDetailInfo = async () => {
-  const res = await fetch('https://softeer.shop/main/car');
+  const res = await fetch(`${baseURL}/main/car`);
   return res.json();
 };
