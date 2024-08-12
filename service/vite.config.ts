43b89c5e13481c2ed6a9f0e2334415e145f4d6e5
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-12 10:28:49 +0900

    chore : proxy 설정 제거

--- service/vite.config.ts
+++ service/vite.config.ts
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.SITE_API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
   cacheDir: './.yarn/.vite',
   resolve: {
     alias: {
