ddb1174232338235cc586abc8ca7058412e3a2b7
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-14 11:20:56 +0900

    chore : 캘린더 한국어 설정

--- admin/src/components/ui/calendar.tsx
+++ admin/src/components/ui/calendar.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { DayPicker } from 'react-day-picker';
+import { ko } from 'date-fns/locale'; // 한국어 로케일 추가
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
@@ -15,7 +16,9 @@ function Calendar({
 }: CalendarProps) {
   return (
     <DayPicker
+      locale={ko}
       showOutsideDays={showOutsideDays}
+      weekStartsOn={1}
       className={cn('p-3', className)}
       classNames={{
         months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
