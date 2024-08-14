f13495b09d6ca582a0b6943b81e9fa9864090500
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-14 15:55:51 +0900

    feat : 로그인 모달 커스텀 훅 구현

---
+++ service/src/apis/login/query.ts
@@ -0,0 +1,52 @@
+import { useMutation, useQuery } from '@tanstack/react-query';
+import {
+  ConfirmVerificationRequestBody,
+  LoginRequestBody,
+} from '@/types/Authorization/request';
+
+import { sendAuthCode, confirmVerification, login, reissueToken } from './api';
+
+export const useMutationCode = () => {
+  const mutation = useMutation({
+    mutationKey: ['sendCode'],
+    mutationFn: (phoneNumber: string) => sendAuthCode(phoneNumber),
+  });
+
+  return mutation;
+};
+
+export const useMutationCodeVerification = () => {
+  const mutation = useMutation({
+    mutationKey: ['codeVerification'],
+    mutationFn: (body: ConfirmVerificationRequestBody) =>
+      confirmVerification(body),
+  });
+
+  return mutation;
+};
+
+export const useMutationLogin = () => {
+  const mutation = useMutation({
+    mutationKey: ['login'],
+    mutationFn: (body: LoginRequestBody) => login(body),
+  });
+
+  return mutation;
+};
+
+export const useQueryReIssueToken = (
+  accessToken: string,
+  refreshToken: string
+) => {
+  const queryKey = ['reissueToken', accessToken, refreshToken];
+
+  const { data, isLoading } = useQuery({
+    queryKey,
+    queryFn: () => reissueToken(accessToken, refreshToken),
+  });
+
+  return {
+    data,
+    isLoading,
+  };
+};
