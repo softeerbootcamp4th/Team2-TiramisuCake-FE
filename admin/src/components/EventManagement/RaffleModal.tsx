3a61627e01b87e6bb359abb850689edb36e3932e
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-14 12:27:14 +0900

    refactor : 모달, 훅 분리

--- admin/src/components/EventManagement/RaffleModal.tsx
+++ admin/src/components/EventManagement/RaffleModal.tsx
@@ -1,9 +1,7 @@
 'use client';
-import { useEffect, useState } from 'react';
 import { TimePicker } from '@/components/ui/datetime-picker';
 import Modal from '@/components/common/Modal';
-import { format, setHours, setMinutes } from 'date-fns';
-import { DrawRequest } from '@/types/eventType';
+import { useRaffleTimes } from '@/hooks/useRaffleTimes'; // 커스텀 훅 import
 
 interface RaffleModalProps {
   isOpen: boolean;
@@ -11,64 +9,13 @@ interface RaffleModalProps {
   handleSave: (body: DrawRequest) => void;
 }
 
-const initRequest: DrawRequest = {
-  startTime: format(new Date(), 'HH:mm:ss'),
-  endTime: format(new Date(), 'HH:mm:ss'),
-};
-const initialRaffleTimes = {
-  startRaffleTime: undefined as Date | undefined,
-  endRaffleTime: undefined as Date | undefined,
-};
-
 const RaffleModal = ({ isOpen, handleClose, handleSave }: RaffleModalProps) => {
-  const [raffleTimes, setRaffleTimes] = useState(initialRaffleTimes);
-
-  const [drawRequest, setDrawRequest] = useState<DrawRequest>(initRequest);
-
-  useEffect(() => {
-    if (raffleTimes.startRaffleTime) {
-      const twoHoursLater = new Date(raffleTimes.startRaffleTime);
-      twoHoursLater.setHours(raffleTimes.startRaffleTime.getHours() + 2);
-
-      const endOfDay = setMinutes(
-        setHours(raffleTimes.startRaffleTime, 23),
-        59
-      );
-      const finalEndTime = twoHoursLater > endOfDay ? endOfDay : twoHoursLater;
-
-      setRaffleTimes((prevTimes) => ({
-        ...prevTimes,
-        endRaffleTime: finalEndTime,
-      }));
-    }
-  }, [raffleTimes.startRaffleTime]);
-
-  useEffect(() => {
-    if (raffleTimes.startRaffleTime && raffleTimes.endRaffleTime) {
-      setDrawRequest({
-        startTime: format(raffleTimes.startRaffleTime, 'HH:mm:ss'),
-        endTime: format(raffleTimes.endRaffleTime, 'HH:mm:ss'),
-      });
-    }
-  }, [raffleTimes]);
-
-  const handleStartTimeChange = (date: Date | undefined) => {
-    if (date) {
-      setRaffleTimes((prevTimes) => ({
-        ...prevTimes,
-        startRaffleTime: date,
-      }));
-    }
-  };
-
-  const handleEndTimeChange = (date: Date | undefined) => {
-    if (date) {
-      setRaffleTimes((prevTimes) => ({
-        ...prevTimes,
-        endRaffleTime: date,
-      }));
-    }
-  };
+  const {
+    raffleTimes,
+    drawRequest,
+    handleStartTimeChange,
+    handleEndTimeChange,
+  } = useRaffleTimes();
 
   if (!isOpen) return null;
 
