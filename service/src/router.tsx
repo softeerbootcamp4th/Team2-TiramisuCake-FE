89e17a2ff9a5b38ba2dd5c0c4f7df409e027ece3
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-19 13:47:54 +0900

    feat : 404 page, error boundary page 연동

--- service/src/router.tsx
+++ service/src/router.tsx
@@ -5,6 +5,9 @@ import LotteryLoungePage from './pages/LotteryLounge';
 import { ROUTER_PATH } from './constants/lib/constants';
 import QuizLoungePage from './pages/QuizLoungePage';
 import CommentsLoungePage from './pages/CommentsLoungePage';
+import ErrorBoundaryPage from './components/ErrorPage/ErrorBoundaryPage';
+import NotFoundPage from './components/ErrorPage/NotFoundPage';
+import LoadingPage from './components/LoadingPage/LoadingPage';
 
 export const router = createBrowserRouter([
   {
@@ -14,18 +17,31 @@ export const router = createBrowserRouter([
       {
         element: <MainPage />,
         index: true, //기본 경로
+        errorElement: <ErrorBoundaryPage />,
       },
       {
         element: <LotteryLoungePage />,
         path: ROUTER_PATH.LOTTERY_LOUNGE,
+        errorElement: <ErrorBoundaryPage />,
       },
       {
         element: <QuizLoungePage />,
         path: ROUTER_PATH.QUIZ_LOUNGE,
+        errorElement: <ErrorBoundaryPage />,
       },
       {
         element: <CommentsLoungePage />,
         path: ROUTER_PATH.COMMENTS_LOUNGE,
+        errorElement: <ErrorBoundaryPage />,
+      },
+      {
+        element: <LoadingPage />,
+        path: 'huh',
+        errorElement: <ErrorBoundaryPage />,
+      },
+      {
+        element: <NotFoundPage />,
+        path: '*',
       },
     ],
   },
