464633ad49773d832f4b969629349ca6f3999cfe
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-19 18:27:52 +0900

    refactor : 동적/정적 이벤트 정보 fetch api 분리

--- service/src/apis/main/api.ts
+++ service/src/apis/main/api.ts
@@ -1,13 +1,16 @@
 import { BASEURL } from '@/constants/lib/constants';
-import { EventInfoResponse } from '@/types/main/eventInfoType';
 
 export const getCarDetailInfo = async () => {
   const res = await fetch(`${BASEURL}/main/car`);
   return res.json();
 };
 
-export const getEventInfo = async (): Promise<EventInfoResponse> => {
-  const response = await fetch(`${BASEURL}/main/event`);
-  const data: EventInfoResponse = await response.json();
-  return data;
+export const getStaticEventInfo = async () => {
+  const response = await fetch(`${BASEURL}/main/event/static`);
+  return response.json();
+};
+
+export const getDynamicEventInfo = async () => {
+  const response = await fetch(`${BASEURL}/main/event/info`);
+  return response.json();
 };
