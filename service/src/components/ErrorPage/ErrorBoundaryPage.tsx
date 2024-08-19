46adb8101f763aca3594a3a41f21e91f981bfe04
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-19 13:58:12 +0900

    fix : error element 수정

--- service/src/components/ErrorPage/ErrorBoundaryPage.tsx
+++ service/src/components/ErrorPage/ErrorBoundaryPage.tsx
@@ -20,7 +20,7 @@ const ErrorBoundaryPage = () => {
   }, [second, navigate]);
 
   return (
-    <div className='flex flex-col font-semibold snap-y justify-center items-center text-center min-w-screen min-h-screen text-hyundai gap-10'>
+    <div className='flex flex-col font-semibold justify-center items-center text-center min-w-screen min-h-screen text-hyundai gap-10'>
       <p className='text-h-s whitespace-pre-wrap'>
         문제가 발생했습니다. 잠시 후 시도해주세요
       </p>
--- service/src/router.tsx
+++ service/src/router.tsx
@@ -7,37 +7,29 @@ import QuizLoungePage from './pages/QuizLoungePage';
 import CommentsLoungePage from './pages/CommentsLoungePage';
 import ErrorBoundaryPage from './components/ErrorPage/ErrorBoundaryPage';
 import NotFoundPage from './components/ErrorPage/NotFoundPage';
-import LoadingPage from './components/LoadingPage/LoadingPage';
+import LoadingPage from './components/Loading/Loading';
 
 export const router = createBrowserRouter([
   {
     element: <App />,
     path: ROUTER_PATH.MAIN,
+    errorElement: <ErrorBoundaryPage />,
     children: [
       {
         element: <MainPage />,
         index: true, //기본 경로
-        errorElement: <ErrorBoundaryPage />,
       },
       {
         element: <LotteryLoungePage />,
         path: ROUTER_PATH.LOTTERY_LOUNGE,
-        errorElement: <ErrorBoundaryPage />,
       },
       {
         element: <QuizLoungePage />,
         path: ROUTER_PATH.QUIZ_LOUNGE,
-        errorElement: <ErrorBoundaryPage />,
       },
       {
         element: <CommentsLoungePage />,
         path: ROUTER_PATH.COMMENTS_LOUNGE,
-        errorElement: <ErrorBoundaryPage />,
-      },
-      {
-        element: <LoadingPage />,
-        path: 'huh',
-        errorElement: <ErrorBoundaryPage />,
       },
       {
         element: <NotFoundPage />,
