1e620109fc660b15ff77c0c9f701a7e9b22c8424
moana16 <moana16@naver.com>
2024-08-20 16:24:17 +0900

    feat : snap scroll 적용

--- service/src/pages/MainPage.tsx
+++ service/src/pages/MainPage.tsx
@@ -1,6 +1,5 @@
 import { useRef } from 'react';
 import { useTabContext } from '@/store/context/useTabContext';
-import EventSection from '@/components/MainPage/EventSection/EventSection';
 import RendingSection from '@/components/MainPage/RendingSection';
 import CarInfoSection from '@/components/MainPage/CarInfoSection/CarInfoSection';
 import useScrollControl from '@/hooks/MainPage/useScrollControl';
@@ -8,6 +7,9 @@ import FcfsSection from '@/components/MainPage/FcfsSection/FcfsSection';
 import LoadingPage from '@/components/Loading/Loading';
 import { EventInfo } from '@/types/main/eventInfoType';
 import { useDynamicEventInfo, useStaticEventInfo } from '@/apis/main/query';
+import EventIntroduction from '@/components/MainPage/EventSection/EventIntroduction';
+import DrawSection from '@/components/MainPage/DrawSection/DrawSection';
+
 const MainPage = () => {
   const { activeTab, setActiveTab } = useTabContext();
   const { dynamicData, isDynamicLoading } = useDynamicEventInfo();
@@ -28,21 +30,28 @@ const MainPage = () => {
   if (isDynamicLoading || isStaticLoading) return <LoadingPage />;
 
   return (
-    <>
+    <div className='snap-y snap-mandatory overflow-auto h-screen'>
       <div ref={rendingSectionRef}>
         <RendingSection onArrowClick={() => setActiveTab('event')} />
       </div>
       <div ref={eventSectionRef}>
-        <EventSection onArrowClick={() => setActiveTab('ioniq5')} />
+        <EventIntroduction onArrowClick={() => setActiveTab('event')} />
       </div>
       <FcfsSection
         fcfsInfo={dynamicData?.result.fcfsInfo as string}
+        fcfsStartTime={dynamicData?.result.fcfsStartTime as string}
+        eventInfo={staticData?.result.eventInfoList[0] as EventInfo}
+      />
+      <DrawSection
+        totalDrawWinner={dynamicData?.result.totalDrawWinner as string}
+        remainDrawCount={dynamicData?.result.remainDrawCount as string}
         eventInfo={staticData?.result.eventInfoList[1] as EventInfo}
       />
+
       <div ref={carInfoSectionRef}>
         <CarInfoSection />
       </div>
-    </>
+    </div>
   );
 };
 
