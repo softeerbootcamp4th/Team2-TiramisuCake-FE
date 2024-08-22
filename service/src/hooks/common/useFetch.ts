306e925c397c9952c03da61b18c92f17c42d989e
moana16 <moana16@naver.com>
2024-08-22 23:06:48 +0900

    chore : common hook으로 폴더 이동

---
+++ service/src/hooks/common/useFetch.ts
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import { UseFetchOptions } from '@/types/apiType';
+
+interface Response<T> {
+  data: T | null;
+  error: Error | null;
+  loading: boolean;
+}
+
+function useFetch<T>(
+  url: string,
+  { method, params, queryParams, body, headers }: UseFetchOptions
+): Response<T> {
+  //const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+
+      let fullUrl = url;
+      if (params) {
+        Object.keys(params).forEach((key) => {
+          fullUrl = fullUrl.replace(`/${key}`, String(params[key]));
+        });
+      }
+
+      if (queryParams) {
+        const queryString = new URLSearchParams(
+          queryParams as Record<string, string>
+        ).toString();
+        fullUrl += `?${queryString}`;
+      }
+
+      const response = await fetch(fullUrl, {
+        method,
+        headers,
+        body: method !== 'GET' && body ? JSON.stringify(body) : null,
+      });
+
+      return response.json();
+    };
+
+    fetchData();
+  }, [url, method, params, queryParams, body, headers]);
+
+  return { data, error, loading };
+}
+
+export default useFetch;
