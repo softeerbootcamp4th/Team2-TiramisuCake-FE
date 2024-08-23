40b4cb2ad09e62170231d00d93cb38b13e513ba2
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-23 13:47:34 +0900

    fix : 동적 이벤트 api 결과 type 수정

--- service/src/types/main/type.ts
+++ service/src/types/main/type.ts
@@ -39,10 +39,13 @@ interface DynamicEventResult {
   startDate: string;
   endDate: string;
   fcfsInfo: string;
+  drawInfo: string;
   totalDrawWinner: string;
   remainDrawCount: string;
   fcfsHint: string;
   fcfsStartTime: string;
+  drawStartTime: string;
+  drawEndTime: string;
 }
 
 export interface StaticEventInfoResponse extends ResponseType {
