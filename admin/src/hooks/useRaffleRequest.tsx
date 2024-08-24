a6f1054f088600096e9337704075a347af141ec2
fairwheel3027148 <popdarnq5@gmail.com>
2024-08-24 14:36:19 +0900

    fix : 시작,종료 날짜 동일 주간으로 변경 및 종료시간 23시 59분이전으로 변경 ( 유효성 검증 추가)

--- admin/src/hooks/useRaffleRequest.tsx
+++ admin/src/hooks/useRaffleRequest.tsx
@@ -1,60 +1,96 @@
 import { useEffect, useState } from 'react';
-import { format, setHours, setMinutes } from 'date-fns';
+import { format } from 'date-fns';
 import { DrawRequest } from '@/type/eventManagement/type';
 
-const initialRaffleTimes = {
-  startRaffleTime: undefined as Date | undefined,
-  endRaffleTime: undefined as Date | undefined,
+interface InitialRaffleTimes {
+  startDate: string | undefined;
+  endDate: string | undefined;
+  startTime: string | undefined;
+  endTime: string | undefined;
+}
+
+const createDateFromString = (
+  dateString: string | undefined,
+  timeString: string | undefined
+): Date => {
+  try {
+    if (!dateString || !timeString) {
+      throw new Error('dateString or timeString is undefined');
+    }
+
+    const dateObject = new Date(`${dateString}T${timeString}`);
+    // 유효한 날짜인지 확인
+    if (isNaN(dateObject.getTime())) {
+      return new Date();
+    }
+
+    return dateObject;
+  } catch (error) {
+    console.error('Error creating date:', error);
+    return new Date(); // 기본값으로 현재 날짜와 시간을 반환
+  }
 };
 
-export const useRaffleRequest = () => {
-  const [raffleTimes, setRaffleTimes] = useState(initialRaffleTimes);
-  const [drawRequest, setDrawRequest] = useState<DrawRequest>({
-    startTime: format(new Date(), 'HH:mm:ss'),
-    endTime: format(new Date(), 'HH:mm:ss'),
+export const useRaffleRequest = ({
+  startDate,
+  startTime,
+  endDate,
+  endTime,
+}: InitialRaffleTimes) => {
+  const startRaffleTime = createDateFromString(startDate, startTime);
+  const endRaffleTime = createDateFromString(endDate, endTime);
+
+  const [raffleTimes, setRaffleTimes] = useState({
+    startRaffleTime,
+    endRaffleTime,
   });
 
-  useEffect(() => {
-    if (raffleTimes.startRaffleTime) {
-      const twoHoursLater = new Date(raffleTimes.startRaffleTime);
-      twoHoursLater.setHours(raffleTimes.startRaffleTime.getHours() + 2);
-
-      const endOfDay = setMinutes(
-        setHours(raffleTimes.startRaffleTime, 23),
-        59
-      );
-      const finalEndTime = twoHoursLater > endOfDay ? endOfDay : twoHoursLater;
-
-      setRaffleTimes((prevTimes) => ({
-        ...prevTimes,
-        endRaffleTime: finalEndTime,
-      }));
-    }
-  }, [raffleTimes.startRaffleTime]);
+  const [drawRequest, setDrawRequest] = useState<DrawRequest>({
+    startTime: format(raffleTimes.startRaffleTime, 'HH:mm:00'),
+    endTime: format(raffleTimes.endRaffleTime, 'HH:mm:00'),
+  });
 
   useEffect(() => {
     if (raffleTimes.startRaffleTime && raffleTimes.endRaffleTime) {
       setDrawRequest({
-        startTime: format(raffleTimes.startRaffleTime, 'HH:mm:ss'),
-        endTime: format(raffleTimes.endRaffleTime, 'HH:mm:ss'),
+        startTime: format(raffleTimes.startRaffleTime, 'HH:mm:00'),
+        endTime: format(raffleTimes.endRaffleTime, 'HH:mm:00'),
       });
     }
   }, [raffleTimes]);
 
   const handleStartTimeChange = (date: Date | undefined) => {
     if (date) {
+      const adjustedDate = new Date(date);
+      adjustedDate.setSeconds(0);
+      adjustedDate.setMilliseconds(0);
+
       setRaffleTimes((prevTimes) => ({
         ...prevTimes,
-        startRaffleTime: date,
+        startRaffleTime: adjustedDate,
       }));
     }
   };
 
   const handleEndTimeChange = (date: Date | undefined) => {
     if (date) {
+      const adjustedDate = new Date(date);
+
+      // 끝나는 시간이 00:00:00인 경우 23:59:59로 조정
+      if (
+        adjustedDate.getHours() === 0 &&
+        adjustedDate.getMinutes() === 0 &&
+        adjustedDate.getSeconds() === 0
+      ) {
+        adjustedDate.setHours(23, 59, 0);
+      } else {
+        // 초 단위를 00으로 조정
+        adjustedDate.setSeconds(0);
+      }
+
       setRaffleTimes((prevTimes) => ({
         ...prevTimes,
-        endRaffleTime: date,
+        endRaffleTime: adjustedDate,
       }));
     }
   };
